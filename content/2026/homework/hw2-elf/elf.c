68e1b3887c2ea92cdf3c7ebc07a1319bb0d94ab4
manvik99 <manvik1499@gmail.com>
2026-02-04 16:51:32 -0700

    fix

--- content/2026/homework/hw2-elf/elf.c
+++ content/2026/homework/hw2-elf/elf.c
@@ -1,9 +1,3 @@
-int b = 3, c = 4, d;
-
 int quadruple(int a) {
-    return a * c + b * d;
-}
-
-int magic() {
-    return 14 * b;
+    return a * 4;
 }
---
+++ content/2026/homework/hw2-elf/elf1.c
@@ -0,0 +1,5 @@
+int b = 3, c = 4, d;
+
+int quadruple(int a) {
+    return a * c + b * d;
+}
